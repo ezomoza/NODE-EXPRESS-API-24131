364c6aaa823af2ae636037b781780792aa6dbf1c
Emanuel Jose Zomoza <ema_z18@hotmail.com>
2024-06-27 22:00:03 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+// http://localhost:3000/
+app.get('/', (req, res) => {
+  res.send('hola express!!')
+});
+
+app.get('/nosotros', (req, res) => {
+  //login
+  res.sendFile(__dirname + '/private/nosotros.html')
+});
+
+app.get('/frutas', (req, res) => {
+  console.log(req.query);
+  res.sendFile(__dirname + '/frutas.json')
+})
+
+const users = [
+  {id: 1, name: 'user 1'},
+  {id: 2, name: 'user 2'},
+  {id: 3, name: 'user 3'},
+]
+
+app.get('/users/:id', (req, res) => {
+  console.log(req.params.id);
+  const user = users.find((user) => user.id == req.params.id )
+  console.log(user);
+  res.send(user)
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+})
